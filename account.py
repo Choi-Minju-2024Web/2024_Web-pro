fa4353b4ed8bfc7d7e5a084f450b41d0c749237d
Choi-Minju-2024web <0412chmj@gmail.com>
2024-11-30 16:54:30 +0900

    Update account.py

--- account.py
+++ account.py
@@ -5,18 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/account/', methods=['GET', 'POST'])
 def account():
-    # if request.method == 'POST':
-    #    db = sqlite3.connect('Table1.db')
-    #    cursor = db.cursor()
-    #    username = request.form['username']
-    #    password = request.form['password'] #데이터 베이스 연결하고 저장
-    #    try:
-    #        cursor.execute('INSERT INTO user (username,password) VALUES (?, ?)', (username, password))
-    #        db.commit()
-    #    except sqlite3.IntegrityError:
-    #        return "유저 이미 존재"
-    #    db.close()
-    #    return redirect('/account/')
+    if request.method == 'POST':
+       db = sqlite3.connect('Table1.db')
+       cursor = db.cursor()
+       username = request.form['username']
+       password = request.form['password'] #데이터 베이스 연결하고 저장
+       try:
+           cursor.execute('INSERT INTO user (username,password) VALUES (?, ?)', (username, password))
+           db.commit()
+       except sqlite3.IntegrityError:
+           return "유저 이미 존재"
+       db.close()
+       return redirect('/account/')
     return render_template('account.html')
         
 if __name__ == '__main__':
