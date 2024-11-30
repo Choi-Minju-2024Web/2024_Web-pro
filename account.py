a80afce40fcd10e922f13cf363a26299aa694678
Choi-Minju-2024web <0412chmj@gmail.com>
2024-11-30 16:42:36 +0900

    Update account.py

--- account.py
+++ account.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template,request,redirect
 import sqlite3
 
 app = Flask(__name__)
+@app.route('/')
 @app.route('/account/', methods=['GET', 'POST'])
 def account():
-    if request.method == 'POST':
-       db = sqlite3.connect('Table1.db')
-       username = request.form['username']
-       password = request.form['password']
-       conn = sqlite3.connect('user.db') #데이터 베이스 연결하고 저장
-       cursor = conn.cursor()
-       try:
-           cursor.execute('INSERT INTO user (ussername,password) VALUES (?, ?)', (username, password))
-           conn.commit()
-       except sqlite3.IntegrityError:
-           return "유저 이미 존재"
-       conn.close()
-       return redirect('/account/')
+    # if request.method == 'POST':
+    #    db = sqlite3.connect('Table1.db')
+    #    cursor = db.cursor()
+    #    username = request.form['username']
+    #    password = request.form['password'] #데이터 베이스 연결하고 저장
+    #    try:
+    #        cursor.execute('INSERT INTO user (username,password) VALUES (?, ?)', (username, password))
+    #        db.commit()
+    #    except sqlite3.IntegrityError:
+    #        return "유저 이미 존재"
+    #    db.close()
+    #    return redirect('/account/')
     return render_template('account.html')
         
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.debug = True
     app.run(host='127.0.0.1', port=5000)
