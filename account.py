f207a80844370d3dcc0ddd9d908ba012bfdbec5f
Choi-Minju-2024web <0412chmj@gmail.com>
2024-11-30 16:17:18 +0900

    Update account.py

--- account.py
+++ account.py
@@ -2,5 +2,15 @@ from flask import Flask, render_template,request,redirect
 import sqlite3
 
 app = Flask(__name__)
-
 @app.route('/account/', methods=['GET', 'POST'])
+def account():
+    if request.method == 'POST':
+       db = sqlite3.connect('Table1.db')
+       username = request.form['username']
+       password = request.form['password']
+       conn = sqlite3.connect('user.db') #데이터 베이스 연결하고 저장
+       cursor = conn.cursor()
+       
+if __name__ == '_main_':
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000)
