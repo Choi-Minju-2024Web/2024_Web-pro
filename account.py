a703ed5e7b322eae654b5b3a15b10fad23f6a072
Choi-Minju-2024web <0412chmj@gmail.com>
2024-12-02 14:45:13 +0900

    Update account.py

--- account.py
+++ account.py
@@ -1,26 +1,4 @@
-from flask import Flask, render_template,request,redirect
-import sqlite3
-
-app = Flask(__name__)
-@app.route('/')
-@app.route('/account/', methods=['GET', 'POST'])
-def account():
-    if request.method == 'POST':
-       db = sqlite3.connect('Table1.db')
-       cursor = db.cursor()
-       username = request.form['username']
-       password = request.form['password'] #데이터 베이스 연결하고 저장
-       try:
-           cursor.execute('INSERT INTO user (username,password) VALUES (?, ?)', (username, password))
-           db.commit()
-       except sqlite3.IntegrityError:
-           return "유저 이미 존재"
-       db.close()
-       return redirect('/account/')
-    return render_template('account.html')
-        
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='127.0.0.1', port=5000)
-
-#다시 커밋시작
+from flask import Flask, render_template, request, redirect 
+# flask 프레임워크를 가져오고, 템플릿 렌더링, 클라이언트 요청 처리
+# 사용자 다른 URL로 연결
+import sqlite3 #SQLite 데이터베이스를 사용하기 위함
