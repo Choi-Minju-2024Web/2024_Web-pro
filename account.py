0b4ddc38e92e5c8030b2e6a86d707de78de8d672
Choi-Minju-2024web <0412chmj@gmail.com>
2024-12-02 15:53:19 +0900

    함수선언 def account _ 회원가입 데이터 저장 (입력 받은 데이터 가져오기)

--- account.py
+++ account.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template, request, redirect
 # 사용자 다른 URL로 연결
 import sqlite3 #SQLite 데이터베이스를 사용하기 위함
 
+# 경로설정
 app = Flask(__name__)
 @app.route('/')
 @app.route('/account/', methods = ['GET','POST'])
+#함수 선언
+def account():
+   #회원가입 데이터 저장
+   #만약 POST요청이 들어오면
+   if request.method == 'POST':
+      #Table1.db에 연결
+      db = sqlite3.connect('Table1.db')
+      cursor = db.cursor()
+      #html 폼에서 입력받은 값 각각 가져오기
+      username = request.form['username']
+      password = request.form['password']
 
 if __name__ == '__main__':
    app.debug = True
