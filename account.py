981075f951583eb07fc69a06b6ed99cbda6014e7
Choi-Minju-2024web <0412chmj@gmail.com>
2024-12-02 15:28:19 +0900

    py / account페이지와 연결, 5000포트로 실행 (함수는 아직 x)

--- account.py
+++ account.py
@@ -3,3 +3,10 @@ from flask import Flask, render_template, request, redirect
 # 사용자 다른 URL로 연결
 import sqlite3 #SQLite 데이터베이스를 사용하기 위함
 
+app = Flask(__name__)
+@app.route('/')
+@app.route('/account/', methods = ['GET','POST'])
+
+if __name__ == '__main__':
+   app.debug = True
+   app.run(host = '127.0.0.1',port = 5000) #포트 5000에서 실행
