eb133cfd1ea36eb3b6149b89a203c10f64eac3a5
Choi-Minju-2024web <0412chmj@gmail.com>
2024-12-06 21:46:53 +0900

    세션을 사용하여 로그인 유지 (account.py에 세션 코드 추가)

--- account.py
+++ account.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect 
+from flask import Flask, render_template, request, redirect, session
 # flask 프레임워크를 가져오고, 템플릿 렌더링, 클라이언트 요청 처리
 # 사용자 다른 URL로 연결
 import sqlite3 #SQLite 데이터베이스를 사용하기 위함
 
 # 경로설정
 app = Flask(__name__)
+app.secret_key = 'secret_key'
+
 @app.route('/')
 @app.route('/account/', methods = ['GET','POST'])
 #함수 선언
@@ -22,6 +24,8 @@ def account():
       try:
          cursor.execute('INSERT INTO user (username,password) VALUES (?,?)', (username,password))
          db.commit()
+         #로그인 시 세션에 username 저장하기
+         session['username'] = username
       # 만약 유저이름이 중복되면 에러 메시지 반환   
       except sqlite3.IntegrityError:
          return "이미 존재하는 이름입니다."
@@ -30,7 +34,7 @@ def account():
       return redirect('/account/')
    # GET 요청일 경우 account.html 파일 반환해 이용자에게 표시
    return render_template('account.html')   
- 
+
 if __name__ == '__main__':
    app.debug = True
    app.run(host = '127.0.0.1',port = 5000) #포트 5000에서 실행
